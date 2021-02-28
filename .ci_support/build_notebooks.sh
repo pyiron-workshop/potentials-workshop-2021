ffc1b4a54a6d32ad03797a3ee10a2144e546e7eb
Jan Janßen <janssen@mpie.de>
2021-02-28 09:57:39 +0100

    add continous integration

---
+++ .ci_support/build_notebooks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# install
+bash binder/postBuild
+
+# exclude notebooks
+rm -rf ./old_notebooks
+
+# execute notebooks
+current_dir=$(pwd)
+i=0;
+for f in $(find . -name *.ipynb); do
+    cd $(dirname $f);
+    notebook=$(basename $f)
+    papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k "python3" || i=$((i+1));
+    cd $current_dir
+done;
+
+# push error to next level
+if [ $i -gt 0 ]; then
+    exit 1;
+fi;
