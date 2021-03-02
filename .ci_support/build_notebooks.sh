8261fb010ccd583009bd32c46775e74cefbfa38e
Marvin Poul <poul@mpie.de>
2021-03-02 07:27:53 +0100

    Sort notebooks by numbers before executing

--- .ci_support/build_notebooks.sh
+++ .ci_support/build_notebooks.sh
@@ -8,7 +8,7 @@ rm -rf ./old_notebooks
 # execute notebooks
 current_dir=$(pwd)
 i=0;
-for f in $(find . -name *.ipynb); do
+for f in $(find . -name *.ipynb | sort -n); do
     cd $(dirname $f);
     notebook=$(basename $f);
     papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k "python3" || i=$((i+1));
